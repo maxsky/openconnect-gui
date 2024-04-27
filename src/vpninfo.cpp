0f4390ca7165855c2104c24ca62f1ff0a15f6307
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-04-27 18:20:31 +0200

    Display paths with the native separator
    Internally, Qt uses "/" as a path separator. Convert to the native
    path separator before displaying or logging the path. End-users may
    get confused by the non-native separator.
    
    Signed-off-by: Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>

--- src/vpninfo.cpp
+++ src/vpninfo.cpp
@@ -677,7 +677,7 @@ void VpnInfo::logVpncScriptOutput()
         }
         file.close();
         if (file.remove() != true) {
-            Logger::instance().addMessage(QLatin1String("Could not remove ") + tfile + ": " + QString::number((int)file.error()));
+            Logger::instance().addMessage(QLatin1String("Could not remove ") + QDir::toNativeSeparators(tfile) + ": " + QString::number((int)file.error()));
         }
 
         if (ss->get_batch_mode() != true && bannerMessage.isEmpty() == false) {
@@ -692,7 +692,7 @@ void VpnInfo::logVpncScriptOutput()
             }
         }
     } else {
-        Logger::instance().addMessage(QLatin1String("Could not open ") + tfile + ": " + QString::number((int)file.error()));
+        Logger::instance().addMessage(QLatin1String("Could not open ") + QDir::toNativeSeparators(tfile) + ": " + QString::number((int)file.error()));
     }
 }
 
