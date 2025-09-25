33af48c2efc3addf11f66850e1a7f016fe661868
Max Sky <maxosky@gmail.com>
2025-09-25 19:04:07 +0800

    macOS build passed

--- CMake/Includes/ProjectExternals_qt-solutions.cmake
+++ CMake/Includes/ProjectExternals_qt-solutions.cmake
@@ -1,24 +1,34 @@
 # --------------------------------------------------------------------------------------------------
 # qt-solutions
 # --------------------------------------------------------------------------------------------------
+include_directories(
+    ${CMAKE_BINARY_DIR}/external/src/qt-solutions-${qt-solutions-TAG}/qtbrowserplugin/src
+    ${CMAKE_BINARY_DIR}/external/src/qt-solutions-${qt-solutions-TAG}/qtlockedfile/src
+    ${CMAKE_BINARY_DIR}/external/src/qt-solutions-${qt-solutions-TAG}/qtpropertybrowser/src
+    ${CMAKE_BINARY_DIR}/external/src/qt-solutions-${qt-solutions-TAG}/qtscriptclassic/src
+    ${CMAKE_BINARY_DIR}/external/src/qt-solutions-${qt-solutions-TAG}/qtservice/src
+    ${CMAKE_BINARY_DIR}/external/src/qt-solutions-${qt-solutions-TAG}/qtsingleapplication/src
+)
+
 ExternalProject_Add(qt-solutions-${qt-solutions-TAG}
     PREFIX ${CMAKE_BINARY_DIR}/external/
     INSTALL_DIR ${CMAKE_BINARY_DIR}/external
     CMAKE_ARGS "${CMAKE_ARGS};-DQt6_DIR=${Qt6_DIR};-DCMAKE_BUILD_TYPE=Release;-DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>"
-    
+
     UPDATE_DISCONNECTED 0
     UPDATE_COMMAND ""
-    
+
     GIT_REPOSITORY https://code.qt.io/qt-solutions/qt-solutions.git
     GIT_TAG ${qt-solutions-TAG}
     GIT_SHALLOW 1
-    
-    BUILD_IN_SOURCE 1
-        
+
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/external/src/qt-solutions-${qt-solutions-TAG}
+    BINARY_DIR ${CMAKE_BINARY_DIR}/external/src/qt-solutions-${qt-solutions-TAG}-build
+
     PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
         ${CMAKE_SOURCE_DIR}/CMake/Includes/CMakeLists_qt-solutions.cmake.in
         ${CMAKE_BINARY_DIR}/external/src/qt-solutions-${qt-solutions-TAG}/CMakeLists.txt
-        CMAKE_COMMAND ${CMAKE_CROSS_COMMAND} -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
+        CMAKE_COMMAND ${CMAKE_CROSS_COMMAND}
 
     # Note: ninja-1.8.2 & cmake-3.10.2 failed in find/install this dependency wo next 2 lines :(
     BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/external/lib/libqtsingleapplication.a
